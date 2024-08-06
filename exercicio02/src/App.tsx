a2e79374015b859b78db4b42623a7c8f40e503bf
samuelsadev <samuelsa.dev@gmail.com>
2024-08-06 16:53:58 -0300

    Rotas com o React Router Dom

--- exercicio02/src/App.tsx
+++ exercicio02/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home/Home';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 function App() {
   return (
     <>
-    <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
